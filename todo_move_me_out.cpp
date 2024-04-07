ca3eb46ef788074c1018f6aeea6633d18049d4f2
Rack0t <vurund@ya.ru>
2024-04-07 13:05:22 +0500

    Initial commit

---
+++ todo_move_me_out.cpp
@@ -0,0 +1,10 @@
+//
+// Created by rackot on 06.04.24.
+//
+
+#include "todo_move_me_out.h"
+#include <iostream>
+
+void todo_move_me_out() {
+    std::cout << "This function should be located in the separated source file" << std::endl;
+}
